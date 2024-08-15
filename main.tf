2b0599cd16519083d92b1e2b68a2bb02ab2897af
Nirant R. Sontakke <78961551+NRS08@users.noreply.github.com>
2024-08-16 02:35:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-provider "aws" {
-  region = "us-east-1"  # Set your desired AWS region
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 3.0"
+   }
+ }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Use your desired AMI ID
-  instance_type = "t2.micro"  # Specify the instance type as t2.micro or any other t2 instance type
-
-  tags = {
-    Name = "ExampleInstance12"
-  }
+provider "aws" {
+ region = “us-west-1”
 }
