e4b0caab96a0c4312d22b305efda796a02bbcb92
Nirant R. Sontakke <78961551+NRS08@users.noreply.github.com>
2024-08-18 01:17:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,16 +6,3 @@ terraform {
    }
  }
 }
-
-provider "aws" {
- region = "us-east-1"
-}
-
-resource "aws_instance" "demo" {
- ami = "ami-00831fc7c1e3ddc60"
- instance_type = "t2.micro"
-
- tags = {
-   name = "Demo System"
- }
-}
